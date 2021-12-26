bc1f20adacac9719dabcf302625783082fe0d78d
Abdurahmanov737 <anaraof@gmail.com>
2021-12-26 04:11:41 +0400

    12/26/2021

---
+++ js/extras.js
@@ -0,0 +1,78 @@
+window.onscroll = function () {
+    scrollFunction();
+
+};
+$(window).on("load", function () {
+    setTimeout(() => {
+        $(".myload").fadeOut("slow");
+        $("body").css("overflow-y", "scroll")
+    }, 3500);
+
+});
+// this is for loading effect (end)
+
+
+function scrollFunction() {
+    if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+        $(".scrollBtn").css(
+            "bottom", "5%"
+        )
+
+    } else {
+        $(".scrollBtn").css(
+            "bottom", "-150px"
+        )
+    }
+}
+
+
+$(".scrollBtn").click(function () {
+    toTop();
+
+
+})
+
+function toTop() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}
+
+
+$('.mySlider').slick({
+    dots: true,
+    infinite: true,
+    speed: 300,
+    autoplay:true,
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true,
+          dots: true
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+      // You can unslick at a given breakpoint now by adding:
+      // settings: "unslick"
+      // instead of a settings object
+    ]
+  });
+
+  
